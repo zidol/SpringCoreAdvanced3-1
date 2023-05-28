a6e72afa6159c3aea50b2898eb43e17822fcef37
zidol <ajh3166@naver.com>
2023-05-28 23:16:51 +0900

    스프링 부트 클래스 직접만들기

--- src/main/java/hello/boot/MySpringApplication.java
+++ src/main/java/hello/boot/MySpringApplication.java
@@ -1,6 +1,5 @@
-package hello.embed;
+package hello.boot;
 
-import hello.servlet.HelloServlet;
 import hello.spring.HelloConfig;
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
@@ -9,10 +8,11 @@ import org.apache.catalina.startup.Tomcat;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-public class EmbedTomcatSpringMain {
+import java.util.List;
 
-    public static void main(String[] args) throws LifecycleException {
-        System.out.println("EmbedTomcatSpringMain.main");
+public class MySpringApplication {
+    public static void run(Class configClass, String[] args) {
+        System.out.println("MySpringApplication.main args = " + List.of(args));
 
         //톰캣 설정
         Tomcat tomcat = new Tomcat();
@@ -22,7 +22,7 @@ public class EmbedTomcatSpringMain {
 
         //스프링 컨테이너 생성
         AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
-        appContext.register(HelloConfig.class);
+        appContext.register(configClass);
 
         //스프링 MVC 디스패치 서블릿 생성, 스프링 컨테이너 연결
         DispatcherServlet dispatcher = new DispatcherServlet(appContext);
@@ -31,6 +31,10 @@ public class EmbedTomcatSpringMain {
         Context context = tomcat.addContext("", "/");
         tomcat.addServlet("", "dispatcher", dispatcher);
         context.addServletMappingDecoded("/", "dispatcher");
-        tomcat.start();
+        try {
+            tomcat.start();
+        } catch (LifecycleException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
